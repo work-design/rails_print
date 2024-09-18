fc13c4a1fcad80545c731025842602e792a041e5
qinmingyuan <mingyuan0715@foxmail.com>
2024-09-18 22:35:03 +0800

    jiabo routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,31 @@
 # frozen_string_literal: true
+Rails.application.routes.draw do
+  scope RailsCom.default_routes_scope do
 
-namespace :jia_bo, defaults: { business: 'jia_bo' } do
-  namespace :panel, defaults: { namespace: 'panel' } do
-    root 'home#index'
-    resources :apps do
-      resources :devices do
-        collection do
-          post :sync
+    namespace :jia_bo, defaults: { business: 'jia_bo' } do
+      namespace :panel, defaults: { namespace: 'panel' } do
+        root 'home#index'
+        resources :apps do
+          resources :devices do
+            collection do
+              post :sync
+            end
+            member do
+              patch :test
+            end
+            resources :device_organs
+          end
         end
-        member do
-          patch :test
+      end
+      namespace :admin, defaults: { namespace: 'admin' } do
+        root 'home#index'
+        resources :device_organs do
+          collection do
+            post :scan
+          end
         end
-        resources :device_organs
       end
     end
-  end
-  namespace :admin, defaults: { namespace: 'admin' } do
-    root 'home#index'
-    resources :device_organs do
-      collection do
-        post :scan
-      end
-    end
+
   end
 end
