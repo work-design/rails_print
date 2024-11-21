abeb571f80e9213b0c69623bfe1905cde5053798
qinmingyuan <mingyuan0715@foxmail.com>
2024-11-21 16:44:32 +0800

    organ

--- app/models/jia_bo/ext/organ.rb
+++ app/models/jia_bo/ext/organ.rb
@@ -3,8 +3,11 @@ module JiaBo
     extend ActiveSupport::Concern
 
     included do
-      has_one :device_organ, -> { where(default: true) }, class_name: 'JiaBo::DeviceOrgan'
+      has_one :device_organ, -> { where(aim: 'receipt') }, class_name: 'JiaBo::DeviceOrgan'
       has_one :device, class_name: 'JiaBo::Device', through: :device_organ
+
+      has_one :device_organ_produce, -> { where(aim: 'produce') }, class_name: 'JiaBo::DeviceOrgan'
+      has_one :device_produce, class_name: 'JiaBo::Device', through: :device_organ_produce, source: :device
 
       has_many :device_organs, class_name: 'JiaBo::DeviceOrgan', dependent: :delete_all
       has_many :devices, class_name: 'JiaBo::Device', through: :device_organs
