4d2090edc3e6138ca91fef1d76dea5409f13e123
qinmingyuan <mingyuan0715@foxmail.com>
2025-08-10 22:02:01 +0800

    test with publish

--- app/apis/print/emqx_api.rb
+++ app/apis/print/emqx_api.rb
@@ -8,6 +8,20 @@ module Print
       r['data']
     end
 
+    def publish(topic:, payload:, id:, **options)
+      r = post(
+        'publish',
+        topic: topic,
+        payload: payload,
+        properties: {
+          user_properties: {
+            ids: id
+          }
+        },
+        **options
+      )
+    end
+
     private
     def with_access_token(tries: 2, params: {}, headers: {}, payload: {})
       headers.merge! Authorization: "Basic #{Base64.strict_encode64([@app.key, @app.secret].join(':'))}"
