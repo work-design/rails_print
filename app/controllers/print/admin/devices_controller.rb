3a919eb2acd54aeb9e7d46274d48a6b9e68b2ee0
qinmingyuan <mingyuan0715@foxmail.com>
2025-08-24 13:57:14 +0800

    jia bo printer

--- app/controllers/print/admin/devices_controller.rb
+++ app/controllers/print/admin/devices_controller.rb
@@ -1,17 +1,10 @@
 module Print
   class Admin::DevicesController < Admin::BaseController
-    before_action :set_app, only: [:scan]
     before_action :set_device, only: [:show, :edit, :update, :destroy, :actions, :test]
     before_action :set_new_device, only: [:new, :create]
 
     def index
       @devices = current_organ.devices.includes(:printer)
-    end
-
-    def scan
-      @device = @app.devices.find_or_initialize_by(device_id: params[:result])
-      @device.device_organs.find_or_initialize_by(organ_id: current_organ.id)
-      @device.save
     end
 
     def test
