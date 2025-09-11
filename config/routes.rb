334f3deeddbb28567372aee654674c6c3d94f18a
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-11 20:51:21 +0800

    r

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,29 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  scope RailsCom.default_routes_scope do
-
-    namespace :print, defaults: { business: 'print' } do
-      namespace :panel, defaults: { namespace: 'panel' } do
-        root 'home#index'
-        resources :mqtt_apps
-        resources :jia_bo_apps do
-          resources :jia_bo_printers do
-            collection do
-              post :sync
-            end
-            member do
-              patch :test
-            end
-          end
-        end
-      end
-      namespace :admin, defaults: { namespace: 'admin' } do
-        root 'home#index'
-        resources :devices
+  namespace :print, defaults: { business: 'print' } do
+    namespace :panel, defaults: { namespace: 'panel' } do
+      root 'home#index'
+      resources :mqtt_apps
+      resources :jia_bo_apps do
         resources :jia_bo_printers do
           collection do
-            post :scan
+            post :sync
+          end
+          member do
+            patch :test
           end
         end
-        resources :printers
       end
     end
-
+    namespace :admin, defaults: { namespace: 'admin' } do
+      root 'home#index'
+      resources :devices
+      resources :jia_bo_printers do
+        collection do
+          post :scan
+        end
+      end
+      resources :printers
+    end
   end
 end
