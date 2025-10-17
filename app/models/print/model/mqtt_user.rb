c904c92fbe7bfa2168e13644217eb94b514e9fb3
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-17 22:10:24 +0800

    mqtt user

--- app/models/print/model/mqtt_user.rb
+++ app/models/print/model/mqtt_user.rb
@@ -1,12 +1,16 @@
+require 'bcrypt'
 module Print
   module Model::MqttUser
     extend ActiveSupport::Concern
 
     included do
       attribute :username, :string
-      attribute :password_digest, :string
+      attribute :password_hash, :string
+    end
 
-      has_secure_password validations: false
+    def set_pass!(password, cost: 10)
+      self.password_hash = BCrypt::Password.create(password, cost: cost)
+      self.save
     end
 
   end
