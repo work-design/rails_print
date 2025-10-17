b59b7ac7da963eb646c9e12651cc4390db69d9e1
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-18 00:48:29 +0800

    mtqp

--- app/models/print/model/mqtt_user.rb
+++ app/models/print/model/mqtt_user.rb
@@ -6,11 +6,21 @@ module Print
     included do
       attribute :username, :string
       attribute :password_hash, :string
+      attribute :password, :string
     end
 
     def set_pass!(password, cost: 10)
       self.password_hash = BCrypt::Password.create(password, cost: cost)
       self.save
+    end
+
+    def api
+      MQTT::Client.connect(
+        host: 'work_design-emqx',
+        port: 5,
+        username: username,
+        password: password
+      )
     end
 
   end
