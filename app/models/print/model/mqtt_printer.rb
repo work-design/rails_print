3c911b3a9e143bd7df68be71175c19fa94fda8a8
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-18 21:04:11 +0800

    mqtt

--- app/models/print/model/mqtt_printer.rb
+++ app/models/print/model/mqtt_printer.rb
@@ -14,16 +14,18 @@ module Print
       #belongs_to :mqtt_app
     end
 
-    def mqtt_superuser
-      MqttUser.where(is_superuser: true).take
+    def api
+      return @api if defined? @api
+      superuser = MqttUser.where(is_superuser: true).take
+      @api = superuser.api
     end
 
     def register_success
-      mqtt_superuser.api.publish "#{dev_imei}/unregistered", 'registerSuccess'
+      api.publish "#{dev_imei}/unregistered", 'registerSuccess'
     end
 
     def register_401
-      mqtt_superuser.api.publish "#{dev_imei}/unregistered", 'registerFail@401'
+      api.publish "#{dev_imei}/unregistered", 'registerFail@401'
     end
 
   end
