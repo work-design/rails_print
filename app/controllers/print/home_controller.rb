e69b610ff64323cb30d39987e27dd915161fbe08
qinmingyuan <mingyuan0715@foxmail.com>
2025-11-10 00:40:25 +0800

    printer logic

--- app/controllers/print/home_controller.rb
+++ app/controllers/print/home_controller.rb
@@ -1,7 +1,14 @@
 module Print
   class HomeController < BaseController
+    skip_before_action :verify_authenticity_token, only: [:message]
 
     def message
+      @mqtt_printer = MqttPrinter.find_or_initialize_by(dev_imei: params[:clientid])
+      @mqtt_printer.save
+
+      @mqtt_printer.register_success
+
+      head :ok
     end
 
   end
