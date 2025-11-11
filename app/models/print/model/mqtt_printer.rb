58ab570a5df4085c761f9d5e098b841319723c3d
qinmingyuan <mingyuan0715@foxmail.com>
2025-11-11 10:46:01 +0800

    simpe

--- app/models/print/model/mqtt_printer.rb
+++ app/models/print/model/mqtt_printer.rb
@@ -75,7 +75,7 @@ module Print
       all = [task_size] + task_bytes + payload_size + payload_bytes + TAG + check
       all_size = [all.size].pack('N').bytes.map(&:to_16_str)
 
-      r = (PREFIX + all_size + all).join(' ')
+      r = (PREFIX + all_size + all).join('')
       logger.debug "The Str: #{r}"
 
       api.publish dev_imei, r, false, 2
